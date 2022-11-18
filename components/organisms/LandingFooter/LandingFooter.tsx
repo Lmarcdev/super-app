5eadd40746287ddc1821feafdf45f07691e9e363
lmarcdev <lmarconi.dev@gmail.com>
2022-11-18 21:49:19 +0000

    verify

--- components/organisms/LandingFooter/LandingFooter.tsx
+++ components/organisms/LandingFooter/LandingFooter.tsx
@@ -3,7 +3,7 @@ import { Container, SimpleGrid } from "@chakra-ui/react"
 export const LandingFooter = () => {
     return <footer>
         <Container>
-            < columns={2} spacing={8}></>
+            <SimpleGrid columns={2} spacing={8}></SimpleGrid>
         </Container>
     </footer>
 }
